8dfa766433a2e7b20b360e27348f4bae6682ac18
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-27 17:48:56 +0300

    Ex 27 Solved (Sum of numbers)

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 //3, 5 -> 243 (3⁵)
 
 //2, 4 -> 16
-
+/*
 Console.Write ("Введите число A:");
 int A=int.Parse(Console.ReadLine()!);
 
@@ -18,6 +18,42 @@ int power (int a, int b)
     for (int i =1; i<b;i++)
     {
         result=result*a;
+    }
+    return result;
+}
+*/
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+Console.Write ("Введите число:");
+string numberStr=Console.ReadLine()!; // даже переводить в int не будем, так проще
+
+Console.WriteLine ($"Сумма цифр числа {numberStr} = {getSumOfNumber(numberStr)}");
+Console.WriteLine ($"Сумма цифр числа {numberStr} = {getSumOfNumber2(numberStr)}");
+
+int getSumOfNumber(string str)
+{
+    int result=0;
+    for (int i=0;i<str.Length;i++)
+    {
+        result+= int.Parse(str[i].ToString()); // а теперь переведем, нам же сложить как числа. 
+        //Отвратительная реализация int.Parse в C# - string можно, а char нельзя, хотя string по сути массив char -  абсолютно не логично!!
+    }
+    return result;
+}
+
+int getSumOfNumber2(string str) // так как мы "не проходили" проблему int.Parse решим другим способом
+{
+    int result=0;
+    int temp=int.Parse(str);
+    for (int i=0;i<str.Length;i++)
+    {
+        result+= temp%10; // последняя цифра
+        temp=temp/10;// целлочисленное деление - без послденей цифры
     }
     return result;
 }
