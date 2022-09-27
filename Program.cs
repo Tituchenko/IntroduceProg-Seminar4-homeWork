f262156071832bd15158da7a01f0ee363b923e75
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-27 17:11:06 +0300

    Ex 25 solved (power)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+Console.Write ("Введите число A:");
+int A=int.Parse(Console.ReadLine()!);
+
+Console.Write ("Введите число B:");
+int B=int.Parse(Console.ReadLine()!);
+
+Console.WriteLine ($"{A} в степени {B} = {power(A,B)}");
+
+int power (int a, int b)
+{
+    int result=a;
+    for (int i =1; i<b;i++)
+    {
+        result=result*a;
+    }
+    return result;
+}
